85f1dffa2b3179cf3ba0a8b5c20fc6f01204166f
gung <gungfebrian@gungs-MacBook-Air.local>
2026-01-03 00:18:37 +0800

    minr

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -34,7 +34,7 @@ export default function Contact() {
 	return (
 		<div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
-			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
+			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto pt-8">
 				<div className="w-full max-w-6xl mx-auto mt-32 sm:mt-0">
 					{/* Header Section */}
 					<div className="text-center mb-16">
